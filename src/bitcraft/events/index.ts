c6f5d8b4a265f1cd07a49e80dccc75f4788c8f73
trinitrotoluene <contact@trinit.is>
2025-08-15 00:01:32 +0100

    Typecheck

--- src/bitcraft/events/index.ts
+++ src/bitcraft/events/index.ts
@@ -1,7 +1,3 @@
-export * from "./ClaimsLoadedEvent";
 export * from "./ConnectedEvent";
 export * from "./ConnectionErrorEvent";
 export * from "./DisconnectedEvent";
-export * from "./RecipeAddedEvent";
-export * from "./RecipesLoadedEvent";
-export * from "./RecipeUpdatedEvent";
